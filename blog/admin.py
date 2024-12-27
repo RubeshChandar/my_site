bbe84bea2b18dd0d2217c58db284f8124eff94ac
Rubesh Chandar <rubeshchander.rc@gmail.com>
2024-12-27 18:23:37 +0000

    Final blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Author, Post, Tag, Comment
-# Register your models here.
+from django.contrib.sessions.models import Session
+
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -17,3 +23,4 @@ admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(Session, SessionAdmin)
